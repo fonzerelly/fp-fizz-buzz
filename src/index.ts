d3b593bd28acf0a14e1ce89aeb60ebc271e3713f
fonzerelly <christian.hoerauf@gmail.com>
2021-07-08 23:09:42 +0200

    Martin ist der beste

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
-import { chain, Either, left, mapLeft, right } from "fp-ts/lib/Either"
+import { Either, getApplicativeValidation, left, mapLeft, right } from "fp-ts/lib/Either"
 import { pipe } from "fp-ts/lib/function"
-import { NonEmptyArray } from "fp-ts/lib/NonEmptyArray"
+import { getSemigroup } from "fp-ts/lib/ReadonlyArray"
+import {pipeable} from "fp-ts/lib/pipeable"
 
-    function lift<E, A>(check: (a: A) => Either<E, A>): (a: A) => Either<NonEmptyArray<E>, A> {
+    function lift<E, A>(check: (a: A) => Either<E, A>): (a: A) => Either<ReadonlyArray<E>, A> {
       return a =>
         pipe(
           check(a),
@@ -10,13 +11,41 @@ import { NonEmptyArray } from "fp-ts/lib/NonEmptyArray"
         )
     }
 
-    const isFizz = lift((a: number): Either<string, number> =>
-        a % 3 === 0 ? left('fizz') : right(a)
-    )
-    const isBuzz = lift((a: number): Either<string, number> =>
-        a % 5 === 0 ? left('buzz') : right(a)
-    )
+    const isFizz = (a: number): Either<ReadonlyArray<string>, number> =>
+        a % 3 === 0 ? left(['fizz']) : right(a)
+    
+    const isBuzz = (a: number): Either<ReadonlyArray<string>, number> =>
+        a % 5 === 0 ? left(['buzz']) : right(a)
+    
 
-    const fizzbuzz = (a: number): Either<NonEmptyArray<string>, number> =>
-      pipe(isFizz(a), chain(isBuzz))
-    console.log(fizzbuzz(15))
+    
+    // const validate = (num: number): Either<ReadonlyArray<string>, number> => {
+    //   const V = getApplicativeValidation((getSemigroup<string>()))
+    //   const id = (num: number): number => num
+    //   const liftedFunction = V.of(id)
+    //   const afterFizz = V.ap(liftedFunction, isFizz(num))
+    //   const afterBuzz = V.ap(afterFizz, isBuzz(num))
+    //   return afterBuzz
+    // }
+
+    const validate = (num: number): Either<ReadonlyArray<string>, number> => {
+      const V = getApplicativeValidation(getSemigroup<string>());
+      const P = pipeable(V);
+      const id = (a: number) => (_: number) => a
+      // return pipe(
+      //   V.of(id),
+      //   P.ap(isFizz(num)),
+      //   P.ap(isBuzz(num))
+      // )
+      return pipe(
+        V.of((a:number) => a),
+        P.apFirst(isFizz(num)),
+        P.ap(isBuzz(num))
+      )
+      
+    }
+
+    console.log(validate(2))
+    console.log(validate(3))
+    console.log(validate(5))
+    console.log(validate(15))
